43ae26dbc7dbd2043bb707aeb87c7a0137442687
AshvithaSaravanan <ashvithasaravanan26@gmail.com>
2024-09-20 18:55:37 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,23 @@
+function insert(num) {
+    document.getElementById('result').value += num;
+}
+
+function clearScreen() {
+    document.getElementById('result').value = '';
+}
+
+function deleteLast() {
+    let value = document.getElementById('result').value;
+    document.getElementById('result').value = value.substring(0, value.length - 1);
+}
+
+function calculate() {
+    try {
+        let expression = document.getElementById('result').value;
+        if (expression) {
+            document.getElementById('result').value = eval(expression);
+        }
+    } catch (e) {
+        document.getElementById('result').value = 'Error';
+    }
+}
